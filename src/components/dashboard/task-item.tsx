3e5ed7dbf10c84a35f9d7a43c9f4537f434c973c
Basu Dev <dev628140@gmail.com>
2025-07-19 23:57:06 +0000

    All the 6 action buttons in Today's Task will always remain visible by d

--- src/components/dashboard/task-item.tsx
+++ src/components/dashboard/task-item.tsx
@@ -271,7 +271,7 @@ export default function TaskItem({
             )}
         </div>
       </div>
-      <div className={cn("flex items-center transition-opacity")}>
+      <div className={cn("flex items-center opacity-0 transition-opacity group-hover:opacity-100")}>
         <div className="flex flex-wrap w-[160px] justify-end">
           <div className="flex">
             {isDraggable && !task.completed && (
